80d6e7d8c300ac466759d7a52315ee3265780bc6
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2021-12-15 10:26:59 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import img from './img-ornament.svg';
+import img3 from './img-ornamentInactive.svg';
+import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./custom.css";
+/*import responsiveImage1 from './title.jpg?sizes[]=300,sizes[]=600,sizes[]=1024,sizes[]=2048';*/
+import responsiveImage1 from './rsp-StBarbara.jpg?sizes[]=300,sizes[]=600,sizes[]=800';
+
+/*window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.min.js');*/
+
+/*var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})*/
