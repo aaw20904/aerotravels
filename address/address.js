784ab19a6495531f2f986c8e7b0cfeb30c00af42
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2021-12-15 10:28:05 +0300

    Add files via upload

---
+++ address/address.js
@@ -0,0 +1,8 @@
+import './img-bird.svg';
+import './img-ornament.svg';
+import './img-angle.svg';
+import w003 from "./address.css";
+//import resp2 from './rsp-about.jpg?sizes[]=150,sizes[]=320,sizes[]=480,sizes[]=640,sizes[]=800,sizes[]=2048';
+//import www1 from './rsp-stVarvara.jpg?sizes[]=150,sizes[]=320,sizes[]=480,sizes[]=640,sizes[]=800,sizes[]=2048';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "./custom.css";
---
+++ photo/photo.js
@@ -0,0 +1,15 @@
+import './img-bird.svg';
+import './img-ornament.svg';
+import './img-angle.svg';
+import './pictures/img-01.jpg';
+import './pictures/img-02.jpg';
+import './pictures/img-03.jpg';
+import './pictures/img-04.jpg';
+import './pictures/img-05.jpg';
+import './pictures/img-06.jpg';
+import w003 from "./photo.css";
+/*import resp2 from './rsp-about.jpg?sizes[]=150,sizes[]=320,sizes[]=480,sizes[]=640,sizes[]=800,sizes[]=2048';*/
+/*import www1 from './rsp-stVarvara.jpg?sizes[]=150,sizes[]=320,sizes[]=480,sizes[]=640,sizes[]=800,sizes[]=2048';*/
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "./custom.css";
+window.bootstrap.carousel = require('bootstrap/js/src/carousel.js');
